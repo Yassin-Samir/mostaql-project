3fcd91fd96a6d77954735e847a9f10426762aee4
yassin samir <yassinsam3@gmail.com>
2022-12-27 18:01:28 +0200

    adding a link

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,6 +1,9 @@
-import { MainPage } from "../../components";
+import { MainPage, LinkComponent } from "../../components";
 const TourismPage = () => (
-  <MainPage NavRoute={"احداث"} collectionName={"events"} title={"events"} />
+  <>
+    <MainPage NavRoute={"احداث"} collectionName={"events"} title={"events"} />{" "}
+    <LinkComponent Route={"events/create"}>اضف</LinkComponent>
+  </>
 );
 
 export default TourismPage;
--- pages/tourism-plans/index.js
+++ pages/tourism-plans/index.js
@@ -1,10 +1,11 @@
-import { MainPage } from "../../components";
+import { MainPage,LinkComponent } from "../../components";
 const TourismPage = () => (
+  (<>
   <MainPage
     NavRoute={"خطط سياحية"}
     collectionName={"tourism-plans"}
     title={"tourism Plans"}
-  />
+  /><LinkComponent Route={'tourism-plans/create'}>اضف</LinkComponent> </>)
 );
 
 export default TourismPage;
